ed70507e331de3dfe04711c0acd8adbaac959858
Daniela Ovalle <daniovape@hotmail.com>
2022-05-22 20:16:12 -0500

    target a

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -11,9 +11,9 @@ function Jobs(){
 
                 <div class="section1">
               <div class="property-card">
-                <a href="#">
+
                   <div class="property-image im1">
-                  </div></a>
+                  </div>
                 <div class="property-description">
                   <h5> Jinx Arcane </h5>
                   <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
@@ -22,9 +22,9 @@ function Jobs(){
               </div>
               <div class="section2">
               <div class="property-card">
-                <a href="#">
+
                   <div class="property-image im2">
-                  </div></a>
+                  </div>
                 <div class="property-description">
                   <h5> Studio Ghibli Movies </h5>
                   <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
@@ -33,9 +33,9 @@ function Jobs(){
               </div>
               <div class="section3">
               <div class="property-card">
-                <a href="#">
+
                   <div class="property-image im3">
-                  </div></a>
+                  </div>
                 <div class="property-description">
                   <h5> Coraje </h5>
                   <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
@@ -44,9 +44,9 @@ function Jobs(){
               </div>
               <div class="section4">
               <div class="property-card">
-                <a href="#">
+
                   <div class="property-image im4">
-                  </div></a>
+                  </div>
                 <div class="property-description">
                   <h5> Mil Huellitas </h5>
                   <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
@@ -55,9 +55,9 @@ function Jobs(){
               </div>
               <div class="section5">
               <div class="property-card">
-                <a href="#">
+
                   <div class="property-image im5">
-                  </div></a>
+                  </div>
                 <div class="property-description">
                   <h5> Forja </h5>
                   <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
@@ -66,9 +66,9 @@ function Jobs(){
               </div>
               <div class="section6">
               <div class="property-card">
-                <a href="#">
+
                   <div class="property-image im6">
-                  </div></a>
+                  </div>
                 <div class="property-description">
                   <h5> Animalandia </h5>
                   <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
