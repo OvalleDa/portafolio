e185c78947bfb1c694c3500b0b359cd4c3fa935f
Daniela Ovalle <daniovape@hotmail.com>
2022-05-22 19:58:47 -0500

    Jobs

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,5 +1,6 @@
 import '../css/Jobs.css'
 import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
 function Jobs(){
 
     return(
@@ -7,8 +8,75 @@ function Jobs(){
             <Navbar/>
             <header className="jobs-header">
                 <div className="container-jobs">
-                    <h1>Página Jobs  del portafolio :D</h1>
+
+                <div class="section1">
+              <div class="property-card">
+                <a href="#">
+                  <div class="property-image im1">
+                  </div></a>
+                <div class="property-description">
+                  <h5> Jinx Arcane </h5>
+                  <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
                 </div>
+              </div>
+              </div>
+              <div class="section2">
+              <div class="property-card">
+                <a href="#">
+                  <div class="property-image im2">
+                  </div></a>
+                <div class="property-description">
+                  <h5> Studio Ghibli Movies </h5>
+                  <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
+                </div>
+              </div>
+              </div>
+              <div class="section3">
+              <div class="property-card">
+                <a href="#">
+                  <div class="property-image im3">
+                  </div></a>
+                <div class="property-description">
+                  <h5> Coraje </h5>
+                  <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
+                </div>
+              </div>
+              </div>
+              <div class="section4">
+              <div class="property-card">
+                <a href="#">
+                  <div class="property-image im4">
+                  </div></a>
+                <div class="property-description">
+                  <h5> Mil Huellitas </h5>
+                  <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
+                </div>
+              </div>
+              </div>
+              <div class="section5">
+              <div class="property-card">
+                <a href="#">
+                  <div class="property-image im5">
+                  </div></a>
+                <div class="property-description">
+                  <h5> Forja </h5>
+                  <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
+                </div>
+              </div>
+              </div>
+              <div class="section6">
+              <div class="property-card">
+                <a href="#">
+                  <div class="property-image im6">
+                  </div></a>
+                <div class="property-description">
+                  <h5> Animalandia </h5>
+                  <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
+                </div>
+              </div>
+              </div>
+                </div>
+                <Footer></Footer>
             </header>
         </div>
     );
